afb2631f60f75b1a1beb59338a40be9f7c6f13a9
feddieminas <faidon.dermesonoglou@gmail.com>
2019-12-15 18:43:39 +0200

    updating our app with a shop page, collection preview and collection item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -40,7 +40,7 @@ class Directory extends React.Component {
                   imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                   size: 'large',
                   id: 5,
-                  linkUrl: 'shop/mens'
+                  linkUrl: ''
                 }
               ]
         }
